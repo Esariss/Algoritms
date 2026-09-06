e871662d587c9547fd1593495589e1f590b51fa7
Boka <borisyesayan@gmail.com>
2026-09-06 22:53:29 +0400

    do

---
+++ ConsoleApp1/1.1/ex.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+
+Module Ex
+    Sub Main()
+        Dim a As Integer = Integer.Parse(Console.ReadLine())
+        Dim b As Integer = Integer.Parse(Console.ReadLine())
+        Dim c As Integer = Integer.Parse(Console.ReadLine())
+
+        Dim max As Integer = a
+
+        If b > max Then
+            max = b
+        End If
+
+        If c > max Then
+            max = c
+        End If
+
+        Console.WriteLine(max)
+    End Sub
+End Module
+
---
+++ ConsoleApp1/1.1/ex2.vb
@@ -0,0 +1,19 @@
+﻿
+Imports System
+
+Module Ex2
+    Sub Main()
+        Dim text As String = Console.ReadLine()
+        Dim isPolidrome As Boolean = True
+
+        For i As Integer = 0 To text.Length \ 2 - 1
+            If text(i) <> text(text.Length - 1 - i) Then
+                isPolidrome = False
+                Exit For
+            End If
+        Next
+
+        Console.WriteLine(isPolidrome)
+    End Sub
+End Module
+
